757358171f3db7c6d9b53bb083dc3fcbfed9b777
Jazz Siddiqui <alisidd@icloud.com>
2025-10-23 17:35:06 -0700

    Initial Commit

---
+++ Emoji Art/Emoji_ArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_ArtApp.swift
+//  Emoji Art
+//
+//  Created by Jazz Siddiqui on 2025-10-23.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_ArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Emoji ArtTests/Emoji_ArtTests.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_ArtTests.swift
+//  Emoji ArtTests
+//
+//  Created by Jazz Siddiqui on 2025-10-23.
+//
+
+import Testing
+@testable import Emoji_Art
+
+struct Emoji_ArtTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
